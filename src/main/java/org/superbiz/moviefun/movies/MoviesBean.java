53ab40b2b97081bd4a997b44cfcf6b8e6daee164
pivotLab1 <p.j.santos@accenture.com>
2018-10-31 10:55:24 -0600

    log remote

--- src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
+++ src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
@@ -16,6 +16,8 @@
  */
 package org.superbiz.moviefun.movies;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,6 +33,7 @@ public class MoviesBean {
 
     @PersistenceContext
     private EntityManager entityManager;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public Movie find(Long id) {
         return entityManager.find(Movie.class, id);
@@ -38,8 +41,7 @@ public class MoviesBean {
 
     @Transactional
     public void addMovie(Movie movie) {
-        System.err.println("Creating movie with title " + movie.getTitle() +
-            ", and year " + movie.getYear());
+        logger.debug("Creating movie with title {} and year {}", movie.getTitle(),movie.getYear());
 
         entityManager.persist(movie);
     }
